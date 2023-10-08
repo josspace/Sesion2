d199ed0fe34bba3c1ff3bc53f2e36914d773b158
josspace <josspace@josspacem1.home>
2023-10-08 19:09:14 +0200

    Initial Test

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -2,9 +2,23 @@ package pkg;
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int resta(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiplicada(int a, int b) {
+		return a * b;
+	}
+	
+	public static int divide(int a, int b) {
+		if (b==0)
+			return -1;
+		else
+			return (a/b);
 	}
 
 }
